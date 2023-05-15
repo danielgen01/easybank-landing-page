11a9a6448b762642f38b60602f16ed716bc87ece
Daniel <gendik38@gmail.com>
2023-05-15 17:38:10 +0200

    add hover state

--- src/components/RequestButton.tsx
+++ src/components/RequestButton.tsx
@@ -1,6 +1,7 @@
 export function RequestButton({}) {
   return (
-    <button className="bg-gradient-to-r from-lime-green to-bright-cyan px-10 py-3 rounded-full font-bold text-white">
+    <button className="bg-gradient-to-r from-lime-green to-bright-cyan px-10 py-3 rounded-full font-bold text-white
+    hover:opacity-75">
       Request Invite
     </button>
   )
