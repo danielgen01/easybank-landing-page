8b5f330eac68a70e4bddb578854b9283d1f85655
Daniel <gendik38@gmail.com>
2023-05-15 18:18:32 +0200

    add whitespace-nowrap

--- src/components/Testimonials/Testimonial.tsx
+++ src/components/Testimonials/Testimonial.tsx
@@ -8,7 +8,7 @@ const Testimonial: React.FC<TestimonialProps> = ({ icon, text ,title}) => {
   return (
     <figure className="flex flex-col items-center mt-10 gap-5 px-5">
       <img src={icon} alt="" />
-      <h1 className="text-dark-blue font-semibold">{title}</h1>
+      <h1 className="text-dark-blue font-semibold whitespace-nowrap">{title}</h1>
       <p className="text-sm text-center">
         {text}
       </p>
